4b5686fd9666917ed3df8a68b76746cb7df29860
Almir Menezes da Cunha Junior <amcj2@ug3c35.windows.cin.ufpe.br>
2018-10-16 09:32:35 -0300

    Implement euler_aprimorado

--- formulas.py
+++ formulas.py
@@ -1,13 +1,17 @@
 from sympy import Symbol
 
+y = Symbol('y')
+t = Symbol('t')
+
 def eulerFormula(f, yn, tn, h):
-    y = Symbol('y')
-    t = Symbol('t')
     fn = f.evalf(subs={y: yn, t: tn})
     return yn+(fn*h)
 
 def backwardEulerFormula(f, yn, tn1, h):
-    y = Symbol('y')
-    t = Symbol('t')
     fn1 = f.evalf(subs={y: eulerFormula(f, yn, tn1-h, h), t: tn1})
     return yn+(fn1*h)
+
+def improvedEulerFormula(f, yn, tn, h):
+    fn = f.evalf(subs={y: yn, t: tn})
+    fn1 = f.evalf(subs={y: eulerFormula(f, yn, tn, h), t: tn+h})
+    return yn + (fn+fn1)*(h/2)
--- methods.py
+++ methods.py
@@ -46,7 +46,26 @@ def euler_inverso(params):
     o.close()
 
 def euler_aprimorado(params):
-    pass
+    y0 = float(params[0])
+    t0 = float(params[1])
+    h = float(params[2])
+    steps = float(params[3])
+    f = parse_expr(params[4])
+    o = open('output.txt', 'a')
+
+    o.write('Metodo de Euler Aprimorado\n')
+    o.write('y( {0} ) = {1}\n'.format(t0, y0))
+    o.write('h = {0}\n'.format(h))
+    o.write('{0} {1}\n'.format(0, y0))
+
+    i = 0
+    yn = y0
+    while i < steps:
+        yn1 = improvedEulerFormula(f, yn, t0+(h*i), h)
+        o.write('{0} {1}\n'.format(i+1, yn1))
+        yn = yn1
+        i += 1
+    o.close()
 
 def runge_kutta(params):
     pass
