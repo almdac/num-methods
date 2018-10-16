654b883f1f930de76ba0fe2ce0257d3dde9b5621
Almir M. da Cunha <almirmdacunha@gmail.com>
2018-10-15 21:24:11 -0300

    Correct input example and receive paramters euler, euler_inverso, euler_aprimorado, runge_kutta

--- methods.py
+++ methods.py
@@ -1,19 +1,41 @@
 import mpmath
-import sympy
+from sympy.parsing.sympy_parser import parse_expr
 
 def euler(params):
-    pass
+    y0 = params[0]
+    t0 = params[1]
+    h = params[2]
+    steps = params[3]
+    f = parse_expr(params[4])
 
 def euler_inverso(params):
-    pass
+    y0 = params[0]
+    t0 = params[1]
+    h = params[2]
+    steps = params[3]
+    f = parse_expr(params[4])
 
 def euler_aprimorado(params):
-    pass
+    y0 = params[0]
+    t0 = params[1]
+    h = params[2]
+    steps = params[3]
+    f = parse_expr(params[4])
 
 def runge_kutta(params):
-    pass
+    y0 = params[0]
+    t0 = params[1]
+    h = params[2]
+    steps = params[3]
+    f = parse_expr(params[4])
 
 def adam_bashforth(params):
+    pass
+
+def adam_multon(params):
+    pass
+
+def formula_inversa(params):
     pass
 
 def adam_bashforth_by_euler(params):
@@ -28,9 +50,6 @@ def adam_bashforth_by_euler_aprimorado(params):
 def adam_bashforth_by_runge_kutta(params):
     pass
 
-def adam_multon(params):
-    pass
-
 def adam_multon_by_euler(params):
     pass
 
@@ -41,9 +60,6 @@ def adam_multon_by_euler_aprimorado(params):
     pass
     
 def adam_multon_by_runge_kutta(params):
-    pass
-
-def formula_inversa(params):
     pass
 
 def formula_inversa_by_euler(params):
